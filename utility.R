6c4547214905c473a01204206457361c978f45cf
Thais <thais@minet.dk>
2018-02-24 21:19:59 +0000

    update preprocessing of data

--- utility.R
+++ utility.R
@@ -1,6 +1,39 @@
 require(gmodels)
 require(Matrix)
 
+# Loads and merges RNA data from a list of file names and paths
+FileMultiMerge = function(file.names, path) {
+  tissueTypes = c("bladder", "breast", "prostate", "thyroid")
+  path.names = lapply(file.names, function(file.name) {
+    paste(path, file.name, sep = "/")
+  })
+  
+  data.list = lapply(path.names, function(file.name) {
+    read.table(file = file.name, header = TRUE)
+  })
+  
+  data = Reduce(function(x, y) {
+    merge(x, y, by=c('Hugo_Symbol', 'Entrez_Gene_Id'))
+  }, data.list)
+  
+  return(data)
+}
+
+
+# Loads and merges RNA data from all files in given path
+PathMultiMerge = function(path) {
+  file.names = list.files(path = path, full.names = TRUE)
+  data.list = lapply(file.names, function(x) {
+    read.table(file = x, header = TRUE)
+  })
+  
+  data = Reduce(function(x, y) {
+    merge(x, y, by=c('Hugo_Symbol', 'Entrez_Gene_Id'))
+  }, data.list)
+  
+  return(data)
+}
+
 # rnaObj <- setClass("rnaObj", slots =
 #                      c(data = "data.frame", pca.scores = "data.frame", pca.load = "data.frame",
 #                        meta = "data.frame", tissue = "vector", genes = "vector", group = "vector",
@@ -19,16 +52,3 @@ require(Matrix)
 # 
 #             return(object)
 #           })
-
-
-
-MultiMerge = function(path) {
-  file.names = list.files(path = path, full.names = TRUE)
-  data.list = lapply(file.names, function(x) {
-    read.table(file = x, header = TRUE)
-  })
-  
-  Reduce(function(x, y) {
-    merge(x, y, by=c('Hugo_Symbol', 'Entrez_Gene_Id'))
-  }, data.list)
-}
